f11ff3371ea745e3662f1af7174b0f4b878d84b7
Alsamman Baian <baian.alsamman@htl-klu.at>
2026-03-15 01:50:46 +0100

    Aufgabe 2.2.3

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -21,14 +21,15 @@ class LeaderboardControllerTests {
     }
 
     @Test
-    fun test_getLeaderboard_correctScoreSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 15, 10.0)
-        val third = GameResult(3, "third", 10, 15.0)
+    //prüft, ob getLb nach Score absteigend sortiert sind
+    fun test_getLB_correctScoreSorting() {
+        val first = GameResult(1, "Maus", 30, 30.0)
+        val second = GameResult(2, "Katze", 25, 15.0)
+        val third = GameResult(3, "Hund", 10, 15.0)
 
         whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
 
-        val res: List<GameResult> = controller.getLeaderboard()
+        val res: List<GameResult> = controller.getLeaderboard(null) // hier wird Null hinzugefügt
 
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
@@ -38,20 +39,74 @@ class LeaderboardControllerTests {
     }
 
     @Test
-    fun test_getLeaderboard_sameScore_CorrectIdSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 20, 10.0)
-        val third = GameResult(3, "third", 20, 15.0)
+    fun test_getLB_sameScoreANDfastestTime() {
+        // id ist absichtlich nicht sortiert um es wirklich zu testen
+        val fast = GameResult(1, "Maus", 30, 30.0)
+        val Medium = GameResult(3, "Katze", 25, 15.0)
+        val slow = GameResult(2, "Hund", 10, 15.0)
+        val res = controller.getLeaderboard(null)
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
-
-        val res: List<GameResult> = controller.getLeaderboard()
-
-        verify(mockedService).getGameResults()
-        assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        whenever(mockedService.getGameResults()).thenReturn(listOf(medium, slowest, fastest))
+        assertEquals(fast, res[0])
+        assertEquals(medium, res[1])
+        assertEquals(slow, res[2])
     }
 
+    @Test
+    // Neuer Rank test
+    fun test_getLB_Rank() {
+        val list = listOf(
+            GameResult(1, "A", 90, 5.0),
+            GameResult(2, "M", 80, 6.0),
+            GameResult(3, "N", 70, 7.0),
+            GameResult(4, "Z", 60, 8.0),
+            GameResult(5, "Q", 50, 9.0),
+            GameResult(6, "L", 40, 10.0),
+            GameResult(7, "T", 30, 11.0),
+            GameResult(8, "F", 20, 12.0)
+        )
+
+        whenever(mockedService.getGameResults()).thenReturn(list)
+
+        val res = controller.getLeaderboard(5)
+
+        assertEquals(7, res.size)
+        assertEquals("M", res[0].playerName)
+        assertEquals("F", res[6].playerName)
+    }
+
+    @Test
+    // rank = -1 somit ist er unglültigt weil kleiner als 1 ist
+    fun test_getLB_invaildRank_throws400() {
+        whenever(mockedService.getGameResults()).thenReturn(emptyList())
+
+        assertFailsWith<ResponseStatusException> {
+            controller.getLeaderboard(-1)
+        }
+    }
+
+    @Test
+    // rank ist hier = 0 und ist ungültigt
+    fun test_getLeaderboard_rankZero_throws400() {
+        whenever(mockedService.getGameResults()).thenReturn(
+            listOf(GameResult(1, "sisi", 90, 5.0))
+        )
+
+        assertFailsWith<ResponseStatusException> {
+            controller.getLeaderboard(0)
+        }
+    }
+
+    @Test
+    // hier wird getest --> rank ist größer --> also hier habt man kein 5 Plätze
+    fun test_getLB_RankLarge_throws400() {
+        whenever(mockedService.getGameResults()).thenReturn(
+            listOf(GameResult(1, "sisi", 90, 5.0))
+        )
+
+        assertFailsWith<ResponseStatusException> {
+            controller.getLeaderboard(5)
+        }
+    }
 }
+
