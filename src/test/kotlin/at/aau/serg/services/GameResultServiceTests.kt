40aeab9e9dc4e27133f83f5acc25de059ac70eb7
Alsamman Baian <baian.alsamman@htl-klu.at>
2026-03-15 01:49:12 +0100

    Aufgabe 2.2.3

--- src/test/kotlin/at/aau/serg/services/GameResultServiceTests.kt
+++ src/test/kotlin/at/aau/serg/services/GameResultServiceTests.kt
@@ -71,5 +71,34 @@ class GameResultServiceTests {
         assertEquals(gameResult2, res[1])
         assertEquals(2, res[1].id)
     }
+    @Test
+    //@Test sagt schon dass es nur ein Test ist
+    // Es wird hier ein Player erstellt  mit Id , name , score und Zeit
+    // dann in die Liste einfügen
+    // dann wird der Player anhand seiner Id gelöscht --> schau Zeile 83
+    // Zeile 86 --> hier wird geprüft, wen n man nach einem Player suchen soll jetzt null zurückkommen --> weil er gelöscht wurde : )
+    fun test_deleteGameResult_existingId_removesEntry() {
+        val gameResult = GameResult(0, "player1", 50, 55.5)
+        service.addGameResult(gameResult)
+
+        service.deleteGameResult(gameResult.id)
+
+        assertNull(service.getGameResult(gameResult.id))
+    }
+
+    @Test
+    // Also hier wird getestet--> wenn man eine Id löschen möchte, welche gar nicht exsistiert
+    //wieder ein Spiel wird erstellt und zu Liste hinzugefügt
+    // Die List hat Eintrag 1
+    fun test_deleteGameResult_nonexistentId() {
+        val gameResult = GameResult(0, "player1", 50, 55.5)
+        service.addGameResult(gameResult)
+        // So hier versucht man diese Id zu Löschen, die gibt aber gar nicht
+        // diese L ist für Long
+        service.deleteGameResult(99L)
+        // Die Lsit hat weiterhin 1 Eintrag --> also das löschen einer nicht exsitierten ID nothing kaputt gemacht
+        assertEquals(1, service.getGameResults().size)
+    }
+}
 
 }
